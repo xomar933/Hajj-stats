48a003732fb3a18d2d8be85679b71b6b80e3e579
Omar Alotibi (xomar933) <xomar77@gmail.com>
2023-01-17 14:01:15 +0300

    تحسين عام

--- stats.js
+++ stats.js
@@ -26,27 +26,29 @@ const puppeteer = require('puppeteer-core')
 //  ------------- ملاحظات اسماء  الازرار مالها فايده اذا تبي تحذفها ---------------------------
 // const puppeteer = require('puppeteer')
 // book id
-//*[@id="j_idt180:j_idt181"]/div[1]/div[1]/div/div[2]/span[2]
+//*[@id="j_idt181:j_idt182"]/div[1]/div[1]/div/div[2]/span[2]
 //  stats
 //*[@id="kt_content"]/div[2]/div/div/div/div[1]/div[1]/div/div[1]/span[2]
 // Lmit butt
-//*[@id="j_idt151:primetable:j_id2"]
+//*[@id="j_idt152:primetable:j_id2"]
 //  limt 100
-//*[@id="j_idt151:primetable:j_id2"]/option[4]
+//*[@id="j_idt152:primetable:j_id2"]/option[4]
 // pages text
-//*[@id="j_idt151:primetable_paginator_bottom"]/span[1]
-// *[@id="j_idt151:primetable_paginator_bottom"]/span[2]/a[1]
-//*[@id="j_idt151:primetable_paginator_bottom"]/span[2]/a[2]
+//*[@id="j_idt152:primetable_paginator_bottom"]/span[1]
+// *[@id="j_idt152:primetable_paginator_bottom"]/span[2]/a[1]
+//*[@id="j_idt152:primetable_paginator_bottom"]/span[2]/a[2]
 
-//*[@id="j_idt180:j_idt181"]/div[2]/div[1]/div/div[1]/span[2]
+//*[@id="j_idt181:j_idt182"]/div[2]/div[1]/div/div[1]/span[2]
 // main div
-//*[@id="j_idt180:j_idt181"]
+//*[@id="j_idt181:j_idt182"]
 // otp input
 //*[@id="j_verifyCode"]
 //  pages
-//*[@id="j_idt151:primetable_paginator_bottom"]/span[2]/a[2]
+//*[@id="j_idt152:primetable_paginator_bottom"]/span[2]/a[2]
 
-// *[@id="j_idt151:primetable_data"]/tr[3]/td[14]/button/i`
+// *[@id="j_idt152:primetable_data"]/tr[3]/td[14]/button/i`
+// عدد الحجاج
+//*[@id="j_idt152:primetable_data"]/tr[1]/td[14]/button/i
 //  ------------- ملاحظات اسماء  الازرار مالها فايده اذا تبي تحذفها ---------------------------
 
 const reader = require('xlsx')
@@ -60,7 +62,7 @@ for (const sheetName of file.SheetNames) {
   worksheets[sheetName] = reader.utils.sheet_to_json(file.Sheets[sheetName]);
 }
 
-
+let HajjsNumber
 var rows;
 (async () => {
   let launchOptions = {
@@ -68,7 +70,7 @@ var rows;
     // args: ['--no-sandbox', "--disabled-setupid-sandbox"],
     headless: false,
     // executablePath: "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe",
-    executablePath: "C:/Program Files (x86)/chromium/chrome.exe",
+    executablePath: process.env.browserUrl,
     args: ['--start-maximized']
   };
   console.log("1")
@@ -89,19 +91,35 @@ var rows;
     console.log(url)
     await page.goto(url)
     // await page.reload();
+    await page.waitForXPath(`//*[@id="j_idt152:primetable:j_id2"]`)
     console.log("4")
-    await page.select('xpath///*[@id="j_idt151:primetable:j_id2"]', '100').catch(exception => {
-      dddd(page, row, true);
+    //*[@id="j_idt152:primetable:j_id2"]
+    //*[@id="j_idt152:primetable:j_id2"]/option[4]
+    //*[@id="j_idt156"]/div/div/div/div/div/a[1]
+    await page.select('xpath///*[@id="j_idt152:primetable:j_id2"]', '100').catch(exception => {
+      dddd(page, 1, true);
       console.log(`element not shown: ${exception}`)
       return
     }
     );
     console.log("limit done")
-
-
-    dddd(page, 1)
+    //*[@id="j_idt152:primetable_paginator_bottom"]/span[1]
+    var BookId_selector = await page.waitForXPath(`//*[@id="j_idt152:primetable_paginator_bottom"]/span[1]`).catch(exception => {
+      dddd(page, row, true);
+      console.log(`element not shown: ${exception}`)
+      return
+    }
+    );
+    var OrdersLengthString = await page.evaluate(element => element.textContent, BookId_selector).catch(exception => {
+      dddd(page, row, true);
+      console.log(`element not shown: ${exception}`)
+      return
+    }
+    );
+    HajjsNumber = OrdersLengthString.split(" ")[3] || null
+    dddd(page, 1, false)
   }
-    , 18 * 1000)
+    , 25 * 1000)
   console.log("3")
 })();
 var url
@@ -111,8 +129,12 @@ let rows_total = 0;
 let repeated_total = 0;
 async function dddd(page, _row, timer) {
   var row = _row
+  if (row >= HajjsNumber && HajjsNumber != null) {
+    console.log("تم الانتهاء من العملية افحص ملف الاكسل")
+    return
+  }
   if (wait == true) {
-    console.log("````````````````````````` wait 293-219-233-092193-0219- `````````")
+    console.log("````````````````````````` الرجاء الانتضار او اعادة فتح البرنامج `````````")
     // setTimeout(() => {
     //   dddd(page, _row, true)لا  
     // }, 1000);
@@ -124,33 +146,32 @@ async function dddd(page, _row, timer) {
       wait = false
     }, 1000);
   }
-  await page.waitForXPath(`//*[@id="j_idt151:primetable_data"]/tr[${row}]/td[14]/button/i`);
-  var statusSelcetor = await page.waitForXPath(`//*[@id="j_idt151:primetable_data"]/tr[${row}]/td[13]`);
-  var statusText = await page.evaluate(element => element.textContent, statusSelcetor)
   if (row >= 100) {
     pageIndex = pageIndex + 1
     row = 1
+    console.log(`^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^`);
+    console.log(`تم قلب الى الصفحه ${pageindex}`);
+    console.log(`^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^`);
   }
   if (pageIndex != 1) {
-    // console.log(`^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^`);
-    // console.log(pageIndex);
-    // console.log(`^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^`);
-    await page.waitForXPath(`//*[@id="j_idt151:primetable_paginator_bottom"]/span[2]/a[${pageIndex}]`);
-    await page.click(`xpath///*[@id="j_idt151:primetable_paginator_bottom"]/span[2]/a[${pageIndex}]`).catch(exception => {
+    await page.waitForXPath(`//*[@id="j_idt152:primetable_paginator_bottom"]/span[2]/a[${pageIndex}]`);
+    await page.click(`xpath///*[@id="j_idt152:primetable_paginator_bottom"]/span[2]/a[${pageIndex}]`).catch(exception => {
       dddd(page, row, true);
       console.log(`element not shown: ${exception}`)
       return
     }
     );
   }
+  var statusSelcetor = await page.waitForXPath(`//*[@id="j_idt152:primetable_data"]/tr[${row}]/td[13]`);
+  var statusText = await page.evaluate(element => element.textContent, statusSelcetor)
   if (statusText != "غير مؤكد") {
     dddd(page, row + 1)
-    console.log("skipped" + row)
+    console.log("تخطي مكرر" + row)
     return
   }
 
   console.log("5")
-  var BookId_selector = await page.waitForXPath(`//*[@id="j_idt151:primetable_data"]/tr[${row}]/td[1]`).catch(exception => {
+  var BookId_selector = await page.waitForXPath(`//*[@id="j_idt152:primetable_data"]/tr[${row}]/td[1]`).catch(exception => {
     dddd(page, row, true);
     console.log(`element not shown: ${exception}`)
     return
@@ -162,7 +183,8 @@ async function dddd(page, _row, timer) {
     return
   }
   );
-  var id_selector = await page.waitForXPath(`//*[@id="j_idt151:primetable_data"]/tr[${row}]/td[2]`).catch(exception => {
+
+  var id_selector = await page.waitForXPath(`//*[@id="j_idt152:primetable_data"]/tr[${row}]/td[2]`).catch(exception => {
     dddd(page, row, true);
     console.log(`element not shown: ${exception}`)
     return
@@ -176,36 +198,36 @@ async function dddd(page, _row, timer) {
   }
   );
   if ((worksheets.Sheet1.filter(x => x.id === id_Text && x["Book_id"] == BookId_Text).length >= 1)) {
-    console.log("repated skipped all : " + repeated_total)
+    console.log("تم التخطي اجمالي التخطي هو : " + repeated_total)
     dddd(page, row + 1)
     return
   } {
 
 
 
-    await page.waitForXPath(`//*[@id="j_idt151:primetable_data"]/tr[${row}]/td[14]/button/i`).catch(exception => {
+    await page.waitForXPath(`//*[@id="j_idt152:primetable_data"]/tr[${row}]/td[14]/button/i`).catch(exception => {
       // dddd(page, row, true);
       console.log(`element not shown: ${exception}`)
       // return
     }
     );
-    await page.click(`xpath///*[@id="j_idt151:primetable_data"]/tr[${row}]/td[14]/button/i`).catch(exception => {
+    await page.click(`xpath///*[@id="j_idt152:primetable_data"]/tr[${row}]/td[14]/button/i`).catch(exception => {
       // dddd(page, row, true);
       console.log(`element not shown: ${exception}`)
       // return
     }
     );
     console.log("5.1")
-    await page.waitForXPath(`//*[@id="j_idt151:primetable_data"]/tr[${row}]/td[14]/ul/li/a`).catch(exception => {
+    await page.waitForXPath(`//*[@id="j_idt152:primetable_data"]/tr[${row}]/td[14]/ul/li/a`).catch(exception => {
       // dddd(page, row, true);
       console.log(`element not shown: ${exception}`)
       // return
     }
     );
     console.log("5.2")
-    handles = await page.$(`xpath///*[@id="j_idt151:primetable_data"]/tr[${row}]/td[14]/ul/li/a`);
+    handles = await page.$(`xpath///*[@id="j_idt152:primetable_data"]/tr[${row}]/td[14]/ul/li/a`);
     await page.evaluate(b => b.click(), handles)
-      // await page.click(`xpath///*[@id="j_idt151:primetable_data"]/tr[${row}]/td[14]/ul/li/a`)
+      // await page.click(`xpath///*[@id="j_idt152:primetable_data"]/tr[${row}]/td[14]/ul/li/a`)
       .catch(exception => {
         // dddd(page, row, true);
         console.log(`element not shown: ${exception}`)
@@ -226,16 +248,19 @@ async function dddd(page, _row, timer) {
     }
     );
     console.log(BookId_Text)
-    await page.waitForXPath(`//*[@id="j_idt180:j_idt181"]`)
-    let len = await page.$$('#j_idt180\\:j_idt181 > *');
+    await page.waitForXPath(`//*[@id="j_idt181:j_idt182"]`)
+
+    let len = await page.$$('#j_idt181\\:j_idt182 > *');
     console.log(`------------------`)
-    console.log(len)
+    console.log(` عدد الاشخاص في الحجز :  ` + len)
     console.log(`------------------`)
 
     for (let i = 1; i <= len.length; i++) {
       console.log(`i : ` + i)
-      console.log("8")
-      var Status_selector = await page.waitForXPath(`//*[@id="j_idt180:j_idt181"]/div[${i}]/div[1]/div/div[2]/span[2]`).catch(exception => {
+      // console.log("8")
+      //*[@id="j_idt181:j_idt182"]/div[1]/div[1]/div/div[2]/span[2]
+      //*[@id="j_idt181:j_idt182"]/div[1]/div[1]/div/div[1]/span[2]
+      var Status_selector = await page.waitForXPath(`//*[@id="j_idt181:j_idt182"]/div[${i}]/div[1]/div/div[2]/span[2]`).catch(exception => {
         dddd(page, row, true);
         console.log(`element not shown: ${exception}`)
         return
@@ -248,7 +273,7 @@ async function dddd(page, _row, timer) {
       }
       );
       console.log("8.1")
-      var id_selector = await page.waitForXPath(`//*[@id="j_idt180:j_idt181"]/div[${i}]/div[1]/div/div[1]/span[2]`).catch(exception => {
+      var id_selector = await page.waitForXPath(`//*[@id="j_idt181:j_idt182"]/div[${i}]/div[1]/div/div[1]/span[2]`).catch(exception => {
         dddd(page, row, true);
         console.log(`element not shown: ${exception}`)
         return
@@ -262,7 +287,7 @@ async function dddd(page, _row, timer) {
       }
       );
       console.log("8.2")
-      console.log(id_Text)
+      // console.log(id_Text)
 
       console.log(worksheets.Sheet1.filter(x => x.id === id_Text && x["Book_id"] == BookId_Text).length)
       if ((worksheets.Sheet1.filter(x => x.id === id_Text && x["Book_id"] == BookId_Text).length >= 1))
@@ -273,7 +298,7 @@ async function dddd(page, _row, timer) {
       // }))
       {
         repeated_total = repeated_total + 1
-        console.log("repated : " + repeated_total)
+        console.log("رقم مكرر : " + repeated_total)
       } else {
         rows_total = rows_total + 1;
         worksheets.Sheet1.push({
@@ -283,10 +308,10 @@ async function dddd(page, _row, timer) {
         });
 
         console.log(`*********************`)
-        console.log(rows_total)
-        console.log(row)
-        console.log(pageIndex)
-        console.log(`---`)
+        console.log(`اجمالي الحجوزات التي اضافتها للكشف هو : ${rows_total}`)
+        console.log(`رقم الصف هو : ` + row)
+        console.log(`الصفحه : ${pageIndex}`)
+        console.log(`البيانات المسجله هي :`)
         console.log({
           "id": id_Text,
           "Book_id": BookId_Text,
@@ -299,9 +324,10 @@ async function dddd(page, _row, timer) {
 
     reader.utils.sheet_add_json(file.Sheets["Sheet1"], worksheets.Sheet1)
     reader.writeFile(file, './result.xlsx');
-    console.log("write done")
+    console.log("تم الحفظ")
     // back home
-    await page.click(`xpath///*[@id="j_idt155"]/div/div/div/div/div/a[1]`).catch(exception => {
+    //*[@id="j_idt156"]/div/div/div/div/div/a[1]
+    await page.click(`xpath///*[@id="j_idt156"]/div/div/div/div/div/a[1]`).catch(exception => {
       dddd(page, row, true);
       console.log(`element not shown: ${exception}`)
       return
